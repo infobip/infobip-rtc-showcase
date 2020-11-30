9d458865e49b52d38a2d972699d8116230752cba
Valida Hamidovic <Valida.Hamidovic@infobip.com>
2020-11-30 14:22:19 +0000

    Pull request #33: Vhamidovic WEBRTC-1191 Use local sdk
    Merge in WEBRTC/infobip-rtc-showcase from vhamidovic-WEBRTC-1191-Use_local_sdk to master
    
    Squashed commit of the following:
    
    commit 25fe5aacb00359c49506a8b658b011d3c38fa2ae
    Author: vhamidovic <validahamidovic@gmail.com>
    Date:   Mon Nov 30 15:08:19 2020 +0100
    
        Release version
    
    commit 6591456deb98d49cddbbae7d81bb189d26dac53e
    Author: vhamidovic <validahamidovic@gmail.com>
    Date:   Thu Nov 26 11:09:17 2020 +0100
    
        Use local sdk
    
    commit ad222aedf1a13e8d448aa87d1b93a99631ff9cd3
    Author: vhamidovic <validahamidovic@gmail.com>
    Date:   Thu Nov 26 11:09:00 2020 +0100
    
        Use local sdk
    
    commit 69059586309b95c657a5aef9959e7e3d4749e330
    Author: vhamidovic <validahamidovic@gmail.com>
    Date:   Thu Nov 26 11:03:34 2020 +0100
    
        Use local sdk

--- react_native/metro.config.js
+++ react_native/metro.config.js
@@ -1,10 +1,20 @@
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
+const {
+  applyConfigForLinkedDependencies,
+} = require('@carimus/metro-symlinked-deps');
+
+module.exports = applyConfigForLinkedDependencies(
+    {
+      transformer: {
+        getTransformOptions: async () => ({
+          transform: {
+            experimentalImportSupport: false,
+            inlineRequires: false,
+          },
+        }),
       },
-    }),
-  },
-};
+    },
+    {
+      projectRoot: __dirname,
+      blacklistLinkedModules: ['react-native'],
+    },
+);
